903047dba3e57dbbabb21f55f32eea24f9c8d30c
Daniel Andrade <danielmarista@gmail.com>
2018-08-10 14:12:00 -0300

    Endereço do API

--- src/properties/config.ts
+++ src/properties/config.ts
@@ -1,7 +1,7 @@
 export default {
   
-   _url: 'http://127.0.0.1:8000/api',
-  //_url: 'https://api-lightrecicla.avsi.eco.br/api',
+  // _url: 'http://127.0.0.1:8000/api',
+   _url: 'https://api-lightrecicla.avsi.eco.br/api',
    _client_id: '2',
    _client_secret: 'qfbcOhLQWEJCc4AWrCEspugtWBJGY8Zhpk1lSmuy',
   
